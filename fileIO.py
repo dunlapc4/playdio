4b2d629c4ca07992c4950ca01c4c2c90bce92a45
Christopher Teters <cteters@gmail.com>
2020-05-15 16:48:04 -0700

    Consolidated some duplicate code to fileIO.py

---
+++ fileIO.py
@@ -0,0 +1,29 @@
+from scipy.io import wavfile
+import wave
+
+def file_input(fileName):
+
+    fs, data = wavfile.read(fileName + '.wav', 'r')
+
+    obj = wave.open(fileName + '.wav', 'wb')
+    obj.setnchannels(1)     # sets mono
+    obj.setsampwidth(2)     # sets to 16-bits
+    obj.setframerate(fs)    # sets sample rate
+
+    return fs, data, obj
+
+def file_output(fileName, fs, data):
+
+    numChan = 1
+    obj = wave.open(fileName + '.wav', 'wb')
+    obj.setnchannels(numChan)   # sets mono
+    obj.setsampwidth(2)         # sets to 16-bits
+    obj.setframerate(fs)        # sets sample rate
+
+    bitDepth = 2                # seems to work. Thought it would be 16
+
+    duration = len(data)/(fs * numChan * bitDepth)
+
+    for i in range(int(duration * fs)):
+        obj.writeframesraw(data)
+    obj.close()
