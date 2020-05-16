2affd22aa624bada85ea3eed15937f282d6cb2fc
Christopher Teters <cteters@gmail.com>
2020-05-16 16:01:32 -0700

    added an audio mixer to IO

--- fileIO.py
+++ fileIO.py
@@ -1,5 +1,6 @@
 from scipy.io import wavfile
 import wave
+import numpy as np
 
 def file_input(fileName):
 
@@ -27,3 +28,30 @@ def file_output(fileName, fs, data):
     obj.writeframesraw(data)
     obj.close()
 
+def combine_audio(file1, file2):
+    # provide from:
+    # https://stackoverflow.com/questions/4039158/mixing-two-audio-files-together-with-python
+
+    fnames =['audioclips/' + file1 + '.wav', 'audioclips/' + file2 + '.wav']
+    wavs = [wave.open(fn) for fn in fnames]
+    frames = [w.readframes(w.getnframes()) for w in wavs]
+
+    # here's efficient numpy conversion of the raw byte buffers
+    # '<i2' is a little-endian two-byte integer.
+    samples = [np.frombuffer(f, dtype='<i2') for f in frames]   # interpret buffer as 1d arr
+    samples = [samp.astype(np.float64) for samp in samples]
+
+    # mix as much as possible
+    n = min(map(len, samples))
+    mix = samples[0][:n] + samples[1][:n]
+
+    #file_output('mix', 48000.0, mix)
+
+    # Save the result
+    mix_wav = wave.open('audioclips/mix.wav', 'w')
+    mix_wav.setparams(wavs[0].getparams())
+    #print(type(mix_wav))
+
+    # before saving, we want to convert back to '<i2' bytes:
+    mix_wav.writeframes(mix.astype('<i2').tobytes())
+    mix_wav.close()
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from effect import delay
 from synth import sine, sawtooth
+import fileIO
 
 fileName = 'sample'
-duration = 1.0          # length in seconds
+audioTest = 'gc'
+duration = 5.0          # length in seconds
 fs = 48000.0            # sample rate, Hz
 freq = 480              # sine freq, Hz, f
 level = .25             # percent of volume level
 
-#sine.sinWav(fileName, duration, fs, freq, level)
-sawtooth.sawWav(fileName, fs, freq)
+sine.sinWav(fileName, duration, fs, freq, level)
+#sawtooth.sawWav(fileName, fs, freq)
+fileIO.combine_audio(fileName, audioTest)
+
 
 mix = .5        # dry & wet ratio of delay effect
 feedback = 1    # number of repeats made
