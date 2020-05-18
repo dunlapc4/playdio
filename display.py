e1eb00875f696e9c568952e622e55d792e896f1d
Christopher Teters <cteters@gmail.com>
2020-05-18 15:48:32 -0700

    Added a button for creating sine waves

--- display.py
+++ display.py
@@ -41,10 +41,52 @@ class Window(tk.Frame):
         menu.add_cascade(label='Help', menu=help)
 
         # sine wave option window
+        #sineWindow = tk.Toplevel(master=self)
 
-        sineButton = tk.Button(self, text='Create Sine Wave',
-                               command=self.sine_window)
-        sineButton.grid(row=11, column=1)
+        sineButton = tk.Button(text='Create a sine Wave',
+                               command=self.create_sin)
+        sineButton.pack()
+
+    def create_sin(self):
+        sin = tk.Toplevel()
+        sin.iconbitmap('images/note.ico')
+        sin.geometry('300x350')
+        sin.title("sine settings")
+
+        tk.Label(sin, text='name').pack()
+        name = tk.Entry(sin)
+        name.pack()
+
+        # duration of clip
+        duration = tk.DoubleVar()
+        tk.Label(sin, text='duration').pack()
+        scaleDuration = tk.Scale(sin, from_=0.0, to=20.0, variable=duration, orient=tk.HORIZONTAL)
+        scaleDuration.pack()
+
+        # sample rate
+        fs = tk.DoubleVar()
+        tk.Label(sin, text='sample rate').pack()
+        scaleFs = tk.Scale(sin, from_=8000.0, to=48000.0, variable=fs, orient=tk.HORIZONTAL)
+        scaleFs.pack()
+
+        # frequency of wave length
+        freq = tk.DoubleVar()
+        tk.Label(sin, text='frequency').pack()
+        scaleFreq = tk.Scale(sin, from_=20.0, to=1200.0, variable=freq, orient=tk.HORIZONTAL)
+        scaleFreq.pack()
+
+        # amplitude of wave
+        level = tk.DoubleVar()
+        tk.Label(sin, text='sound level').pack()
+        scaleLevel = tk.Scale(sin, from_=0, to=100, variable=level, orient=tk.HORIZONTAL)
+        scaleLevel.pack()
+
+        submitSine = tk.Button(sin, text='Submit',
+                               command=sine.sinWav(name.get(),
+                               duration.get(), fs.get(), freq.get(),
+                               level.get()))
+        submitSine.pack()
+
 
     def client_exit(self):
         exit()
@@ -58,47 +100,6 @@ class Window(tk.Frame):
         sample = tk.Label(self, text='sample input')
         sample.grid(row=4, column=4)
         sample.pack()
-
-
-    def sine_window(self):
-
-        def submit_sine():
-            sine.sinWav(name.get(),
-                        duration.get(), fs.get(), freq.get(),
-                        level.get())
-
-        sineWindow = tk.Toplevel(app)
-
-        tk.Label(sineWindow, text='name').pack()
-        name = tk.Entry(sineWindow)
-        name.pack()
-
-        duration = tk.DoubleVar()
-        tk.Label(sineWindow, text='duration').pack()
-        scaleDuration = tk.Scale(sineWindow, from_=0.0, to=20.0, variable=duration, orient=tk.HORIZONTAL)
-        scaleDuration.pack()
-
-
-        fs = tk.DoubleVar()
-        tk.Label(sineWindow, text='sample rate').pack()
-        scaleFs = tk.Scale(sineWindow, from_=8000.0, to=48000.0, variable=fs, orient=tk.HORIZONTAL)
-        scaleFs.pack()
-
-
-        freq = tk.DoubleVar()
-        tk.Label(sineWindow, text='frequency').pack()
-        scaleFreq = tk.Scale(sineWindow, from_=20.0, to=1200.0, variable=freq, orient=tk.HORIZONTAL)
-        scaleFreq.pack()
-
-        level = tk.DoubleVar()
-        tk.Label(sineWindow, text='sound level').pack()
-        scaleLevel = tk.Scale(sineWindow, from_=0, to=100, variable=level, orient=tk.HORIZONTAL)
-        scaleLevel.pack()
-
-        submitSine = tk.Button(sineWindow, text='Submit',
-                               command=self.submit_sine).pack()
-        submitSine.pack()
-
 
 root = tk.Tk()
 root.iconbitmap('images/note.ico')
